ad6d901c8c704c6f67aba5cd5fa0fdc4e00c5cbc
Natalia Tsed <106387902+NataliaTsed@users.noreply.github.com>
2023-05-24 18:29:18 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
-    text = 'Для начала работы Вам необходимо ввести данные через пробел в следующем формате: \n' \
-        ' \n1) Название валюты, цену которой Вы хотите узнать;  \n2) Название валюты, в которой Вы хотите узнать ' \
+    text = 'Для начала работы введи данные через пробел в следующем формате: \n' \
+        ' \n1) Название валюты, цену которую хочешь узнать;  \n2) Название валюты, в которой хочешь узнать ' \
            'цену первой валюты; \n3) Количество первой валюты.\n' \
            '\nПример ввода конверсии 100 долларов в евро: доллар евро 100 \n\
  \nСписок доступных валют: /values'
 
-    bot.send_message(message.chat.id, "Дорогой пользователь! \nДобро пожаловать в лучший валютный конвертер! :-)")
+    bot.send_message(message.chat.id, "Дорогой пользователь!🤗 \n"
+                                      "Добро пожаловать в руки Гуру валютных операций! 💱 ")
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEJF4hkbhqTI295Ogk2ZGZQYwbUyFdccwAC2wADmL-ADTKdOUX4uisaLwQ")
     bot.reply_to(message, text)
 
 
 @bot.message_handler(commands=['values'])
 def values_def(message: telebot.types.Message):
-    text = 'Вам доступны следующие валюты:'
+    text = 'Рад предложить следующие виды валют:'
     for key in keys.keys():
         text = '\n'.join((text, key,))
     bot.reply_to(message, text)
@@ -33,23 +35,26 @@ def get_price(message: telebot.types.Message):
         values = message.text.split(' ')
 
         if len(values) != 3:
-            raise APIException('Неверно введены параметры! Необходимо ввести 3 значения!')
+            raise APIException('Ты ввел много данных! Для расчета мне необходимо лишь 3 значения!')
 
         base, quote, amount = values
         total_base = CurrencyCalculator.get_price(base, quote, amount)
         if float(amount) == 0:
-            raise APIException('Введите сумму больше нуля! Иначе я не смогу Вам помочь! :-)')
+            raise APIException('Введи сумму больше нуля! Иначе я не смогу тебе помочь! :-)')
         if float(amount) <= 0:
-            raise APIException('Введите положительное количество! В кредит не меняем! :-)')
+            raise APIException('Введи положительное количество! В кредит не меняю! :-)')
 
     except APIException as e:
-        bot.reply_to(message, f'Ошибка пользователя. \n{e}')
+        bot.reply_to(message, f'Ошибка пользователя 😳. \n{e}')
 
     except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+        bot.reply_to(message, f'Не удалось обработать команду 😵‍💫\n{e}')
     else:
         text = f'Стоимость {amount} {base} в {quote}: {round(float(amount) / float(total_base), 2)}'
         bot.send_message(message.chat.id, text)
+        bot.send_message(message.chat.id, 'С тобой приятно иметь дело! 🤝')
+        bot.send_message(message.chat.id, 'Буду ждать тебя еще! 👀')
+        bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEJF5xkbiDrkPoQb-KvSF-HokVBpqefiAACyQADmL-ADZ3o05MJ4JudLwQ")
 
 
 bot.polling(none_stop=True)
--- exceptions.py
+++ exceptions.py
@@ -11,27 +11,28 @@ class CurrencyCalculator:
     @staticmethod
     def get_price(base: str, quote: str, amount: str):
         if quote == base:
-            raise APIException(f'Введите различные валюты: {base}.')
+            raise APIException(f'Надо ввести различные валюты: {base}.')
         if base == quote:
-            raise APIException(f'Введите различные валюты: {quote}.')
+            raise APIException(f'Надо ввести различные валюты: {quote}.')
+        if base.isdigit():
+            raise APIException("В названии валют цифр нет! ;-) ")
+        if quote.isdigit():
+            raise APIException("В названии валют цифр нет! ;-) ")
         # quote_ticker, base_ticker = keys[quote], keys[base]
         try:
             base_ticker = keys[base]
         except KeyError:
             raise APIException(f'Введено некорректное название валюты {base}! Мне такая валюта не известна!')
-        except ValueError:
-            raise APIException("В названии валют цифр нет! ;-) ")
-
         try:
             quote_ticker = keys[quote]
         except KeyError:
             raise APIException(f'Введено некорректное название валюты {quote}! Выберите валюту из /values')
-        except ValueError:
-            raise APIException("В названии валют цифр нет! ;-) ")
+#        except ValueError:
+#            raise APIException("В названии валют цифр нет! ;-) ")
         try:
             amount = float(amount)
         except ValueError:
-            raise APIException(f'Не удалось обработать количество {amount}! Вводите только цифры!')
+            raise APIException(f'Не удалось обработать количество {amount} ! Вводи только цифры!')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
